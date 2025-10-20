ef30cececdfd101a5a4dbb69e2c505fa7cc80b4c
Ashtin Gebert <jayster365@gmail.com>
2025-10-19 23:30:52 -0500

    Ch09 Labs
    Dosent work what so ever the video is very unclear in what im supposed to do i followed along with every step minus what you said not to and now it dosent work

---
+++ Labs/CH09/Auctions/Auctions/Data/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Auctions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Listing> Listings { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Prevent multiple cascade delete paths
+            builder.Entity<Bid>()
+                .HasOne(b => b.Listing)
+                .WithMany(l => l.Bids)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.Listing)
+                .WithMany(l => l.Comments)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Listing>()
+                .HasOne(l => l.User)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Bid>()
+                .HasOne(b => b.User)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Comment>()
+                .HasOne(c => c.User)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
