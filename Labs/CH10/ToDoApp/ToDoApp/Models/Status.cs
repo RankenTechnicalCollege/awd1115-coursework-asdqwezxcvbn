2678d57e3f27c0dda2275f4be0304a67d5a7d71b
Ashtin Gebert <jayster365@gmail.com>
2025-10-30 15:11:40 -0500

    Update 3

--- Labs/CH10/ToDoApp/ToDoApp/Models/Status.cs
+++ Labs/CH10/ToDoApp/ToDoApp/Models/Status.cs
@@ -2,7 +2,7 @@
 {
     public class Status
     {
-        public string StatusId { get; set; } 
-        public string Name { get; set; }
+        public string StatusId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
     }
 }
--- Labs/CH10/ToDoApp/ToDoApp/Models/ToDo.cs
+++ Labs/CH10/ToDoApp/ToDoApp/Models/ToDo.cs
@@ -8,23 +8,23 @@ namespace ToDoApp.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Please Enter a Description")]
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please Enter A Due Date")]
         public DateTime? DueDate { get; set; }
 
         [Required(ErrorMessage = "Please Select A Category")]
-        public string CategoryId { get; set; }
+        public string CategoryId { get; set; } = string.Empty;
 
         [ValidateNever]
-        public Category Category { get; set; }
+        public Category Category { get; set; } = null!;
 
         [Required(ErrorMessage = "Please Select A Status")]
         public string StatusId { get; set; } = string.Empty;
 
         [ValidateNever]
 
-        public Status Status { get; set; } = null;
+        public Status Status { get; set; } = null!;
 
         public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
 
